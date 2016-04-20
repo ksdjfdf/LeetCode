e5502e678fae332082335f71bf661b332da26182
ksdjfdf <330773102@qq.com>
2016-04-20 09:11:49 +0800

    Add two numbers

--- 2_AddTwoNumbers/Solution.h
+++ 2_AddTwoNumbers/Solution.h
@@ -9,11 +9,10 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry = 0, sum = 0;
+        int carry = 0;
 		ListNode *ptr1 = l1, *ptr2 = l2, *result, *current;
-		sum = (ptr1->val + ptr2->val) % 10;
+		result = new ListNode((ptr1->val + ptr2->val) % 10);
 		carry = (ptr1->val + ptr2->val) / 10;
-		result = new ListNode(sum);
 		current = result;
 		ptr1 = ptr1->next;
 		ptr2 = ptr2->next;
@@ -24,21 +23,18 @@ public:
 				}
 				return result;
 			} else if (ptr1 != NULL && ptr2 != NULL) {
-				sum = (ptr1->val + ptr2->val + carry) % 10;
+				current->next = new ListNode((ptr1->val + ptr2->val + carry) % 10);
 				carry = (ptr1->val + ptr2->val + carry) / 10;
-				current->next = new ListNode(sum);
 				ptr1 = ptr1->next;
 				ptr2 = ptr2->next;
 			} else {
 				if (NULL == ptr1) {
-					sum = (ptr2->val + carry) % 10;
+					current->next = new ListNode((ptr2->val + carry) % 10);
 					carry = (ptr2->val + carry) / 10;
-					current->next = new ListNode(sum);
 					ptr2 = ptr2->next;
 				} else {
-					sum = (ptr1->val + carry) % 10;
+					current->next = new ListNode((ptr1->val + carry) % 10);
 					carry = (ptr1->val + carry) / 10;
-					current->next = new ListNode(sum);
 					ptr1 = ptr1->next;
 				}
 			}
